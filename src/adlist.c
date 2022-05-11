41c16e27ee1ef67698e65050cee6138e6476f662
tzj-fxz <2000013150@stu.pku.edu.cn>
2022-05-11 19:25:53 +0800

    testaba

--- src/adlist.c
+++ src/adlist.c
@@ -4,7 +4,12 @@
  * All rights reserved.
  *
  * 
+ *
  * 
+ * abaaba
+ * 
+ * 
+ *  
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
