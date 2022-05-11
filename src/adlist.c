a608a3b62ccedeb93f379aef4363818f45a7351f
tzjfxz <2973512917@qq.com>
2022-05-11 21:45:05 +0800

    tzj2

--- src/adlist.c
+++ src/adlist.c
@@ -4,6 +4,7 @@
  * All rights reserved.
  *
  * from desktop
+ * from tzj
  *  
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
