3422867a60661a6962b95ff67d2e550af46cead0
tzjfxz <2973512917@qq.com>
2022-05-11 21:37:28 +0800

    tzj

--- src/adlist.c
+++ src/adlist.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2006-2010, Salvatore Sanfilippo <antirez at gmail dot com>
  * All rights reserved.
  *
- * 
+ * from desktop
  *  
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
