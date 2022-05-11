9534703f249b9311aeb94132eed4cea15b8024f6
tzj-fxz <2000013150@stu.pku.edu.cn>
2022-05-11 20:33:22 +0800

    message2333

--- src/adlist.c
+++ src/adlist.c
@@ -12,6 +12,7 @@
  * abaaba
  * 
  * 2333333
+ * 2333333
  *  
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
