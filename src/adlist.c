73d2ed7013e7a85414596abfb51bdec744fa5a10
tzjfxz <2973512917@qq.com>
2022-05-11 22:02:56 +0800

    win

--- src/adlist.c
+++ src/adlist.c
@@ -5,6 +5,7 @@
  *
  * from desktop
  * from tzj
+ * from win
  *  
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
