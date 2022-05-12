1ad8971f17dcb437724364280bea71f7b8ef76e6
tzj-fxz <2000013150@stu.pku.edu.cn>
2022-05-12 10:49:58 +0800

    tzj edit

--- src/adlist.c
+++ src/adlist.c
@@ -4,6 +4,7 @@
  * All rights reserved.
  *
  * test edit
+ * test tzj edit
  *  
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
